b671ae42097bf173ab2fe6cc856b17ae10112672
twozeronine <amdusias209@gmail.com>
2022-08-25 00:40:08 +0900

    fix : 주석추가 오류 테스트 코드 제거

--- lib/gen_chatting_ets.ex
+++ lib/gen_chatting_ets.ex
@@ -2,6 +2,7 @@ defmodule GenChattingEts do
   use GenServer
 
   def start_link(init_arg) do
+    # 방이름을 갖게하여 오류시에도 방 이름을 포함하여 상태를 저장할수있다.
     room_name = init_arg[:room_name]
     GenServer.start_link(__MODULE__, room_name, name: {:global, room_name})
   end
@@ -21,13 +22,14 @@ defmodule GenChattingEts do
 
   @impl true
   def handle_call({:connect, client_pid}, _from, room_name) do
+    # 상태는 ets에 맡긴다 !
     GenChattingEts.SimpleCache.add(room_name, client_pid)
     {:reply, self(), room_name}
   end
 
   @impl true
   def handle_cast({:send, message}, room_name) do
-    :a = message
+    # 상태는 ets에 맡긴다 !
     client_list = GenChattingEts.SimpleCache.get(room_name)
     Enum.map(client_list, fn client_pid -> send(client_pid, {:message, message}) end)
     {:noreply, room_name}
